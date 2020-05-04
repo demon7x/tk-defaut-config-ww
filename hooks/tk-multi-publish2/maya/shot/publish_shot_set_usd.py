3221521ce2b29ba3ccd5114d43bb06ed373810ae
demon7x <demon7x@westworld.co.kr>
2020-05-04 13:49:55 +0900

    shot grp

--- hooks/tk-multi-publish2/maya/shot/publish_shot_set_usd.py
+++ hooks/tk-multi-publish2/maya/shot/publish_shot_set_usd.py
@@ -349,6 +349,9 @@ def _to_tractor(instance,item,mel_command):
     start_frame, end_frame = _find_scene_animation_range()
     tractor = to_tractor.MayaToTractor(item)
     tractor.create_script(mel_command)
+
+    tractor.create_add_frame_script(mel_command,start_frame,end_frame)
+    #tractor.create_script(mel_command)
     tractor.to_tractor(start_frame,end_frame,file_type)
 
 
