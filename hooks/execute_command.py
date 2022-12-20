4fc29f3f7536da4f50c908b36431b947c786f1b2
shinkm1104 <shinkm1104@gmail.com>
2022-12-20 10:34:17 +0900

    시간 데이터 수정

--- hooks/execute_command.py
+++ hooks/execute_command.py
@@ -49,7 +49,7 @@ class TimeLogManager():
             if user_status == "WORKING" and log_shot == shot_name:
                 res = QtGui.QMessageBox.question(None,
                                                     "타임로그 충돌",
-                                                    "이미 동일한 {0}의 타임로그를 기록중입니다.\n".format( log_shot ) +
+                                                    "이미 동일한 {0}의 타임로그를 기록 중입니다.\n".format( log_shot ) +
                                                     "{0}에 시작된 기록을 취소하고 새로운 로그를 시작할까요?".format( datetime_str ),
                                                     QtGui.QMessageBox.Yes|QtGui.QMessageBox.No|QtGui.QMessageBox.Cancel 
                                                     )
@@ -69,8 +69,8 @@ class TimeLogManager():
             elif user_status == "WORKING" and log_shot != shot_name:
                 res = QtGui.QMessageBox.question(None,
                                                     "타임로그 충돌",
-                                                    "{0}의 타임로그를 기록중입니다.\n".format( log_shot ) +
-                                                    "기존의 기록을 취소하고\n{0}을 기록할까요?".format( shot_name ),
+                                                    "{0}의 타임로그를 기록 중입니다.\n".format( log_shot ) +
+                                                    "기존의 기록을 취소하고 {0}을 기록할까요?".format( shot_name ),
                                                     QtGui.QMessageBox.Yes|QtGui.QMessageBox.No|QtGui.QMessageBox.Cancel 
                                                     )
                 if res == QtGui.QMessageBox.Yes:
@@ -108,7 +108,7 @@ class TimeLogManager():
             
         ### 2. SAVE 또는 SAVE AS를 선택했을 때 ###
         elif operation == "SAVE" or operation == "SAVE_AS" :
-            ### 2-1. 기록중인 타임로그가 없는 경우 ###
+            ### 2-1. 기록 중인 타임로그가 없는 경우 ###
             ### user_info.user_status == "RESTING" ###
             if user_status == "WORKING" and log_shot == shot_name :
                 res = QtGui.QMessageBox.question(None,
@@ -118,8 +118,10 @@ class TimeLogManager():
                                                 )
                 if res == QtGui.QMessageBox.Yes:
                     work_time = ( now - datetime_object )
-                    work_time_str = "{0}:{1}:{2}".format( work_time.seconds//3600, work_time.seconds//60 , work_time.seconds%60 )
-                    work_time_kor = "{0}시간 {1}분 {2}초".format( work_time.seconds//3600, work_time.seconds//60 , work_time.seconds%60 )
+                    work_time_hour = work_time.seconds//3600
+                    work_time_min  = (work_time.seconds - (work_time_hour*3600))//60
+                    work_time_str = "{0}:{1}:{2}".format( work_time_hour, work_time_min , work_time.seconds%60 )
+                    work_time_kor = "{0}시간 {1}분 {2}초".format( work_time.seconds//3600, work_time_min , work_time.seconds%60 )
                     QtGui.QMessageBox.information(None,
                                                         "타임로그 종료",
                                                         "{0}의 타임로그가 종료되었습니다\n{1} 기록".format( file_name, work_time_kor )
@@ -131,12 +133,14 @@ class TimeLogManager():
                     user_status = "RESTING"
                     QtGui.QMessageBox.information(None,
                                                         "타임로그 안내",
-                                                        "현재 기록중인 타임로그는 {0}입니다.\n타임로그가 저장되지 않습니다.".format( log_shot ),
+                                                        "현재 기록 중인 타임로그는 {0}입니다.\n" +
+                                                        "타임로그가 저장되지 않습니다.\n\n".format( log_shot )+
+                                                        "파일은 정상적으로 저장됩니다.",
                                                         )
             else :
                 QtGui.QMessageBox.information(None,
                                                     "타임로그 안내",
-                                                    "기록중인 타임로그가 없습니다.\n" + 
+                                                    "기록 중인 타임로그가 없습니다.\n" + 
                                                     "타임로그 저장 시 툴킷으로 작업을 진행해주세요.\n\n" +
                                                     "파일은 정상적으로 저장됩니다.")
 
